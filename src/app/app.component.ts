eac4c8ea062a650b357157e1994ad270a3d25e6b
sharad <sharad.pawar@pb.com>
2018-10-27 07:35:24 +0530

    home page design changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +7,10 @@ import { Router } from "@angular/router";
 })
 export class AppComponent {
   title = 'app';
-  constructor(private _router: Router){
+  constructor(){
   }
   ngOnInit(){
-    console.log(this._router.url)
+    
   }
   
 }
